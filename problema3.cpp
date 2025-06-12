f49da4ec7be04f5b19fcc0a972300b45911c71f9
pfvilcav <pfvilcav@unjbg.edu.pe>
2025-06-11 19:18:23 -0500

    Problema 3(Intercambiar el orden de los números): Se amplió el límite de 8 a 16 y se modificó la información que brinda el programa al ser ejecutado.

--- problema3.cpp
+++ problema3.cpp
@@ -2,8 +2,8 @@
 using namespace std;
 
 int main() {
-    int n, A[8], max;
-    cout<<"Cuantos numeros ingresara?(max 8): "<<endl;
+    int n, A[16], max;
+    cout<<"Cuantos numeros ingresara?(max: 16): "<<endl;
     cin>>n;
     cout<<"Ingrese los numeros: "<<endl;
     for (int i=0; i<n; i++) {
@@ -15,7 +15,7 @@ int main() {
         A[n-i-1]=max;
     }
     cout<<endl;
-    cout<<"Se cambio el orden de los elementos: "<<endl;
+    cout<<"Se intercambio el orden de los elementos ingresados: "<<endl;
     for (int i=0; i<n; i++) {
         cout<<A[i]<<" ";
     }
